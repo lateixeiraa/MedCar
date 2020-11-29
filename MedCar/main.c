6121d803d0598de089a269980508221010f646b6
Larissa Teixeira <62350941+lateixeiraa@users.noreply.github.com>
2020-11-29 16:27:33 -0300

    Update main.c

--- MedCar/main.c
+++ MedCar/main.c
@@ -5,16 +5,16 @@
  * Author : Larissa e Marina - PC
  */ 
 
-#define F_CPU 16000000UL //Frequência de trabalho da CPU
+#define F_CPU 16000000UL //FrequÃªncia de trabalho da CPU
 #define BAUD 9600
 #define MYUBRR F_CPU/16/BAUD-1
 #include <avr/io.h>
 #include <util/delay.h> // Incluir Delay
-#include <avr/interrupt.h> // Incluir Interrupções
+#include <avr/interrupt.h> // Incluir InterrupÃ§Ãµes
 #include "nokia5110.h" // Biblioteca do display nokia
 #include <avr/eeprom.h> // Incluir EEPROM
 
-// Variáveis Globais
+// VariÃ¡veis Globais
 #define MIN 0.5
 #define MAX 99.9
 
@@ -43,7 +43,7 @@ void Menu()
 	
 }
 
-ISR(INT0_vect) // Sensor ultrassônico
+ISR(INT0_vect) // Sensor ultrassÃ´nico
 {
 	Menu();
 	nokia_lcd_set_cursor(50,40);
@@ -51,7 +51,7 @@ ISR(INT0_vect) // Sensor ultrassônico
 	nokia_lcd_render();
 	_delay_ms(5000);
 	
-	PORTD = 0b00000100;	// Interrompe os motor no tempo do delay
+	PORTD = 0b00000100;	// Interrompe os motores no tempo do delay
 }
 
 ISR(USART_RX_vect)
@@ -164,7 +164,7 @@ ISR(USART_RX_vect)
 		
 	}
 	
-	// Iluminação Corredores
+	// IluminaÃ§Ã£o Corredores
 	
 	if (recebido == 'm' )
 	{
@@ -187,42 +187,42 @@ ISR(USART_RX_vect)
 	USART_Transmit(recebido);
 }
 
-// Função para inicialização da USART
+// FunÃ§Ã£o para inicializaÃ§Ã£o da USART
 void USART_Init(unsigned int ubrr)
 {
-	UBRR0H = (unsigned char)(ubrr>>8); //Ajusta a taxa de transmissão
+	UBRR0H = (unsigned char)(ubrr>>8); //Ajusta a taxa de transmissÃ£o
 	UBRR0L = (unsigned char)ubrr;
 	UCSR0B = (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0); //Habilita o transmissor e o receptor
 	UCSR0C = (1<<USBS0)|(3<<UCSZ00); //Ajusta o formato do frame: 8 bits de dados e 2 de parada
 	
 }
 
-// Função para envio de um frame de 5 a 8bits
+// FunÃ§Ã£o para envio de um frame de 5 a 8bits
 void USART_Transmit(unsigned char data)
 {
-	while(!( UCSR0A & (1<<UDRE0)));//Espera a limpeza do registrador de transmissão
+	while(!( UCSR0A & (1<<UDRE0)));//Espera a limpeza do registrador de transmissÃ£o
 	UDR0 = data; //Coloca o dado no registrador e o envia
 }
 
-// Função para recepção de um frame de 5 a 8bits
+// FunÃ§Ã£o para recepÃ§Ã£o de um frame de 5 a 8bits
 unsigned char USART_Receive(void)
 {
 	while(!(UCSR0A & (1<<RXC0))); //Espera o dado ser recebido
-	return UDR0; //Lê o dado recebido e retorna
+	return UDR0; //LÃª o dado recebido e retorna
 }
 void main(void)
 {
 	//GPIO
-	DDRB  = 0xFF; //Define a porta B como saída
+	DDRB  = 0xFF; //Define a porta B como saÃ­da
 	DDRC  = 0xFF;
-	DDRD =	0b11111000; //PD saídas
-	PORTD = 0b00000100; //Habilitação do pull-up
+	DDRD =	0b11111000; //PD saÃ­das
+	PORTD = 0b00000100; //HabilitaÃ§Ã£o do pull-up
 
-	//Configuração das interrupções
-	EICRA = 0b00000010;//interrupção externa INT0 na borda de descida
-	EIMSK = 0b00000001;//habilita a interrupção externa INT0
+	//ConfiguraÃ§Ã£o das interrupÃ§Ãµes
+	EICRA = 0b00000010;//interrupÃ§Ã£o externa INT0 na borda de descida
+	EIMSK = 0b00000001;//habilita a interrupÃ§Ã£o externa INT0
 
-	TCCR0A = 0b10000011; //PWM não invertido nos pinos OC0A
+	TCCR0A = 0b10000011; //PWM nÃ£o invertido nos pinos OC0A
 	TCCR0B = 0b00000101; //frequencia em 61Hz
 	OCR0A = 0; //controle do ciclo ativo do PWM
 	
@@ -233,7 +233,7 @@ void main(void)
 	//EEPROM
 	char R_array[15],W_array[15] = "DADOS";
 	
-	eeprom_write_block(W_array,0,strlen(W_array)); // Escrever no endereço 0 do EEPROM
+	eeprom_write_block(W_array,0,strlen(W_array)); // Escrever no endereÃ§o 0 do EEPROM
 
 	sei();
 	
@@ -243,7 +243,7 @@ void main(void)
 		Menu();	
 		OCR0A = (dutyCycle*255)/100.0;
 	
-		// Registro de dados de solicitação no EEPROM
+		// Registro de dados de solicitaÃ§Ã£o no EEPROM
 		if(recebido = '1')
 		{
 			cont ++;
